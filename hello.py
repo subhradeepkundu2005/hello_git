ab333c35d19dc5d698e04b073d0948a29ca0df96
Subhradeep Kundu <subhradeepkundu000@gmail.com>
2024-11-24 14:35:25 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform addition and subtraction
+addition = num1 + num2
+subtraction = num1 - num2
+
+# Display the results
+print(f"The addition of {num1} and {num2} is: {addition}")
+print(f"The subtraction of {num1} and {num2} is: {subtraction}")
+
